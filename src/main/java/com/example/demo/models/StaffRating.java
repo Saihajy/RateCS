87e83afc3d3b26094c3e1aa8b0f60656c7d73635
Saihaj Toor <st.812@protonmail.com>
2026-02-11 16:16:25 -0800

    added staffrating class

---
+++ src/main/java/com/example/demo/models/StaffRating.java
@@ -0,0 +1,95 @@
+package com.example.demo.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="staff")
+public class StaffRating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private RoleType role;
+    private int clarity;
+    private int niceness;
+    private int knowledgeableScore;
+    private String comment;
+    private LocalDateTime createdAt;
+
+    public StaffRating(String name, String email, RoleType role, int clarity, int niceness, int knowledgeableScore, String comment) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.clarity = clarity;
+        this.niceness = niceness;
+        this.knowledgeableScore = knowledgeableScore;
+        this.comment = comment;
+    }
+
+    public StaffRating(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getClarity() {
+        return clarity;
+    }
+
+    public void setClarity(int clarity) {
+        this.clarity = clarity;
+    }
+
+    public int getNiceness() {
+        return niceness;
+    }
+
+    public void setNiceness(int niceness) {
+        this.niceness = niceness;
+    }
+
+    public int getKnowledgeableScore() {
+        return knowledgeableScore;
+    }
+
+    public void setKnowledgeableScore(int knowledgeableScore) {
+        this.knowledgeableScore = knowledgeableScore;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    
+
+
+}
